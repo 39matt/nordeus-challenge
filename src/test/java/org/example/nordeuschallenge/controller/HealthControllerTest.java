9704cd5a67fb8a6a7785de5ed546127576269d33
39matt <matija.todosijevic.i4@gmail.com>
2026-05-04 20:10:56 +0200

    edit: health controller and test

---
+++ src/test/java/org/example/nordeuschallenge/controller/HealthControllerTest.java
@@ -0,0 +1,24 @@
+package org.example.nordeuschallenge.controller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(HealthController.class)
+public class HealthControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void health_shouldReturn200() throws Exception{
+        mockMvc.perform(get("/api/v1/health"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.status").value("ok"));
+    }
+}
