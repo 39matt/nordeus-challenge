e228bc24af81090e583532a8b0833ccb3ebdee6f
39matt <matija.todosijevic.i4@gmail.com>
2026-05-04 20:20:58 +0200

    test: health test

--- src/test/java/org/example/nordeuschallenge/controller/HealthControllerTest.java
+++ src/test/java/org/example/nordeuschallenge/controller/HealthControllerTest.java
@@ -3,6 +3,7 @@ package org.example.nordeuschallenge.controller;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.boot.webmvc.test.autoconfigure.WebMvcTest;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
