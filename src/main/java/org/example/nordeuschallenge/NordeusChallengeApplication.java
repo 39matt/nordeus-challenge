45a8ecef093c90f8c95d485130ef0449ee398b48
39matt <matija.todosijevic.i4@gmail.com>
2026-05-04 17:51:13 +0200

    batman

---
+++ src/main/java/org/example/nordeuschallenge/NordeusChallengeApplication.java
@@ -0,0 +1,13 @@
+package org.example.nordeuschallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NordeusChallengeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NordeusChallengeApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/nordeuschallenge/NordeusChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.nordeuschallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NordeusChallengeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
